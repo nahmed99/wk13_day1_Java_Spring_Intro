3400c8e5f938337856a3cc8b7cabb907b3c7a436
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-08 10:05:43 +0000

    initial commit

---
+++ src/test/java/com/example/codeclan/pirateservice/PirateserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.pirateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PirateserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
