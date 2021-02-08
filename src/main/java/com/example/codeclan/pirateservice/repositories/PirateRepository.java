504c0a3b4b29f30bf3df72c141e6f197e9ca1519
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-08 12:11:11 +0000

    tested

--- src/main/java/com/example/codeclan/pirateservice/repositories/PirateRepository.java
+++ src/main/java/com/example/codeclan/pirateservice/repositories/PirateRepository.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Repository;
 // extends the japRepository for Pirate objects, with id (of type Long)
 // to distinguish them - uniquely identify each Pirate object.
 
+// This class will inherit all that it requires from JpaRepository...Spring
+// will implement this interface, so we won't need to.
+
 @Repository
 public interface PirateRepository extends JpaRepository<Pirate, Long> {
 }
--- src/test/java/com/example/codeclan/pirateservice/PirateserviceApplicationTests.java
+++ src/test/java/com/example/codeclan/pirateservice/PirateserviceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.codeclan.pirateservice;
 
+import com.example.codeclan.pirateservice.models.Pirate;
+import com.example.codeclan.pirateservice.repositories.PirateRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PirateserviceApplicationTests {
 
+	@Autowired
+	PirateRepository pirateRepository; // access an instance of PirateRepository
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canCreateAndSavePirate(){
+		// create pirate
+		Pirate pirate = new Pirate("Jack", "Sparrow", 33);
+
+		// get hold of pirate repository and save a pirate object.
+		pirateRepository.save(pirate);
+
+		// don't need an assert in this case - it is being used illustrate a point.
+
 	}
 
 }
